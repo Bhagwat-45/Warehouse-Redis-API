69f9bbbd481cb030a54407f399fe5862081e9ecc
Chinmay S B <cbh.mavim@gmail.com>
2025-12-05 18:15:56 +0530

    Finished Project

--- Store-Microservice/main.py
+++ Store-Microservice/main.py
@@ -1,8 +1,13 @@
+from ast import Or
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from httpx import request
 from more_itertools import quantify
 from redis_om import get_redis_connection,HashModel
 from settings import credentials
+import requests
+from fastapi.background import BackgroundTasks
+import time
 
 app = FastAPI(
     title="Store API"
@@ -23,7 +28,7 @@ redis = get_redis_connection(
 )
 
 class ProductOrder(HashModel):
-    product_id : int
+    product_id : str
     quantity: int
     class Meta:
         database = redis
@@ -31,4 +36,58 @@ class ProductOrder(HashModel):
 class Order(HashModel):
     product_id : str
     price : float
-    fee : 
+    fee : float
+    total : float
+    quantity : int
+    status : str
+    class Meta:
+        database = redis
+
+@app.post('/orders')
+def create(productOrder: ProductOrder,background_task: BackgroundTasks):
+    req = requests.get(f'http://localhost:8000/product/{productOrder.product_id}')
+    product = req.json()
+    fee = product['price'] * 0.2
+
+    order = Order(
+        product_id = productOrder.product_id,
+        price = product['price'],
+        fee = fee,
+        total =  product['price'] + fee,
+        quantity = productOrder.quantity,
+        status = 'pending'
+    )
+    order.save()
+
+    background_task.add_task(order_complete(order))
+    return order
+
+@app.get("/order/{pk}")
+def get_product(pk: str):
+    return Order.get(pk)
+
+@app.get('/orders')
+def get_all_orders():
+    return [format(pk) for pk in Order.all_pks()]
+
+def format(pk:str):
+    order = Order.get(pk)
+    return {
+        'id' : order.pk,
+        'product_id': order.product_id,
+        'fee' : order.fee,
+        'total' : order.total,
+        'quantity' : order.quantity,
+        'status' : order.status
+    }
+
+@app.delete('/order/{pk}')
+def delete(pk:str):
+    return Order.delete(pk)
+
+
+def order_complete(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+
